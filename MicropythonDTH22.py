3f97c7176616d67be06276e1d5554933f3ee41dc
Witsarut Wongsim <row3no6@gmail.com>
2023-06-15 23:06:58 +0700

    Update MicropythonDTH22.py

--- MicropythonDTH22.py
+++ MicropythonDTH22.py
@@ -1,1 +1,11 @@
-
+from machine import Pin
+import dht
+d = dht.DHT22(Pin15))
+for i in range(100):
+  d.measure()
+  time.sleep(1)
+  print(d.temerature())
+  print(d.humidity)
+  time.sleep(3)
+  
+  
